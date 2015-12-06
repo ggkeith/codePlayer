777b66448fcb66958d11af0feafed563c2369fa1
George Keith <ggkeithv@gmail.com>
2015-12-05 23:24:42 -0600

    got html and css working...javascript...not so much

--- scripts.js
+++ scripts.js
@@ -2,5 +2,33 @@
 	var menuBarHeight = $("#menuBar").height();
 	var codeContainerHeight = windowHeight - menuBarHeight;
 
-	
+
 	$(".codeContainer").height(codeContainerHeight + "px");
+
+	$(".toggle").click(function() {
+
+		$(this).toggleClass("selected");
+
+		var activeDiv = $(this).html();
+
+		$("#" + activeDiv + "Container").toggle();
+
+		var showingDivs = $(".codeContainer").filter(function() {
+			return($(this).css("display") != "none");
+		}).length;
+
+		var width = 100/showingDivs;
+
+		$(".codeContainer").width(width + "%");
+	});
+
+	var cssCode = $("#cssCode").val();
+	var htmlCode = $("#htmlCode").val();
+
+	$("#runButton").click(function() {
+		$("iframe").contents().find("html").html('<style>' + cssCode + '</style>') + htmlCode;
+
+
+	eval("#jsCode").val();
+
+	});
